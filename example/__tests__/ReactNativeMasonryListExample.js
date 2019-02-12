bad4805cd89963656ce5145ba41f462433780754
Lue <lue_hang@hotmail.com>
2019-02-11 23:40:11 -0600

    Edit example project testing.

--- example/__tests__/ReactNativeMasonryListExample.js
+++ example/__tests__/ReactNativeMasonryListExample.js
@@ -1,10 +1,10 @@
 import "react-native";
 import React from "react";
-import App from "../App";
+import ReactNativeMasonryListExample from "../ReactNativeMasonryListExample";
 
 // Note: test renderer must be required after react-native.
 import renderer from "react-test-renderer";
 
 it("renders correctly", () => {
-  renderer.create(<App />);
+  renderer.create(<ReactNativeMasonryListExample />);
 });
