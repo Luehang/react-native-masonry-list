a8fb3fdd7d78346fa928534dc9d5be146b02e394
Steve Saul <sjsaul1101@gmail.com>
2020-05-14 14:37:03 -0700

    Update MasonryList.js

--- src/MasonryList.js
+++ src/MasonryList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, InteractionManager } from "react-native";
+import { FlatList, InteractionManager, View } from "react-native";
 import PropTypes from "prop-types";
 
 import { resolveImage, resolveLocal } from "./lib/model";
