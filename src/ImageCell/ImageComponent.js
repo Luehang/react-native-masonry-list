2142a31ceefbc1a1ff7f4352f884c34f92320b36
Sivadatta Kodali <sivadatta.sureshbabu@walgreens.com>
2021-07-16 12:06:45 +0530

    Added data prop to ImageComponent. This would result the data back when clicking on each Imagecell through props.

--- src/ImageCell/ImageComponent.js
+++ src/ImageCell/ImageComponent.js
@@ -4,48 +4,51 @@ import PropTypes from "prop-types";
 import Injector from "./Injector";
 
 export default class ImageComponent extends React.PureComponent {
-	static propTypes = {
-		width: PropTypes.number.isRequired,
-		height: PropTypes.number.isRequired,
-		gutter: PropTypes.number.isRequired,
-		source: PropTypes.any.isRequired,
-		imageContainerStyle: PropTypes.object,
-		customImageComponent: PropTypes.oneOfType([
-			PropTypes.func,
-			PropTypes.object
-		]),
-		customImageProps: PropTypes.object
-	}
+  static propTypes = {
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    gutter: PropTypes.number.isRequired,
+    source: PropTypes.any.isRequired,
+    data: PropTypes.any.isRequired,
+    imageContainerStyle: PropTypes.object,
+    customImageComponent: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.object,
+    ]),
+    customImageProps: PropTypes.object,
+  };
 
-	render() {
-		const {
-			width,
-			height,
-			gutter,
-			source,
-			imageContainerStyle,
-			customImageComponent,
-			customImageProps
-		} = this.props;
-		const imageProps = {
-			source: source,
-			resizeMethod: "auto",
-			style: {
-				width: width,
-				height: height,
-				margin: gutter / 2,
-				backgroundColor: "gainsboro",
-				...imageContainerStyle
-			}
-		};
+  render() {
+    const {
+      width,
+      height,
+      gutter,
+      source,
+      data,
+      imageContainerStyle,
+      customImageComponent,
+      customImageProps,
+    } = this.props;
+    const imageProps = {
+      source: source,
+      resizeMethod: "auto",
+      data: data,
+      style: {
+        width: width,
+        height: height,
+        margin: gutter / 2,
+        backgroundColor: "gainsboro",
+        ...imageContainerStyle,
+      },
+    };
 
-		return (
-			<Injector
-				defaultComponent={Image}
-				defaultProps={imageProps}
-				injectant={customImageComponent}
-				injectantProps={customImageProps}
-			/>
-		);
-	}
+    return (
+      <Injector
+        defaultComponent={Image}
+        defaultProps={imageProps}
+        injectant={customImageComponent}
+        injectantProps={customImageProps}
+      />
+    );
+  }
 }
--- src/ImageCell/TouchableImageComponent.js
+++ src/ImageCell/TouchableImageComponent.js
@@ -4,43 +4,53 @@ import PropTypes from "prop-types";
 import ImageComponent from "./ImageComponent";
 
 export default class TouchableImageComponent extends React.PureComponent {
-	static propTypes = {
-		data: PropTypes.object.isRequired,
-		width: PropTypes.number.isRequired,
-		height: PropTypes.number.isRequired,
-		gutter: PropTypes.number.isRequired,
-		source: PropTypes.any.isRequired,
-		imageContainerStyle: PropTypes.object,
-		customImageComponent: PropTypes.oneOfType([
-			PropTypes.func,
-			PropTypes.object
-		]),
-		customImageProps: PropTypes.object,
-		onPressImage: PropTypes.func,
-		onLongPressImage: PropTypes.func
-	}
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    gutter: PropTypes.number.isRequired,
+    source: PropTypes.any.isRequired,
+    imageContainerStyle: PropTypes.object,
+    customImageComponent: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.object,
+    ]),
+    customImageProps: PropTypes.object,
+    onPressImage: PropTypes.func,
+    onLongPressImage: PropTypes.func,
+  };
 
-	render() {
-		const {
-			data, width, height, gutter, source,
-			imageContainerStyle, onPressImage,
-			onLongPressImage, customImageComponent,
-			customImageProps
-		} = this.props;
-		return (
-			<TouchableOpacity
-				onPress={() => onPressImage && onPressImage(data, data.index)}
-				onLongPress={() => onLongPressImage && onLongPressImage(data, data.index)}>
-				<ImageComponent
-					width={width}
-					height={height}
-					gutter={gutter}
-					source={source}
-					imageContainerStyle={imageContainerStyle}
-					customImageComponent={customImageComponent}
-					customImageProps={customImageProps}
-				/>
-			</TouchableOpacity>
-		);
-	}
+  render() {
+    const {
+      data,
+      width,
+      height,
+      gutter,
+      source,
+      imageContainerStyle,
+      onPressImage,
+      onLongPressImage,
+      customImageComponent,
+      customImageProps,
+    } = this.props;
+    return (
+      <TouchableOpacity
+        onPress={() => onPressImage && onPressImage(data, data.index)}
+        onLongPress={() =>
+          onLongPressImage && onLongPressImage(data, data.index)
+        }
+      >
+        <ImageComponent
+          width={width}
+          height={height}
+          gutter={gutter}
+          source={source}
+          data={data}
+          imageContainerStyle={imageContainerStyle}
+          customImageComponent={customImageComponent}
+          customImageProps={customImageProps}
+        />
+      </TouchableOpacity>
+    );
+  }
 }
